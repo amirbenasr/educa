57967f05081e6b50897c34d5de5f72627c50649f
amir <amir@dragonchain.com>
2023-05-09 14:44:12 +0100

    add video

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,2 +1,6 @@
-export const BASE_URL = "http://localhost/wordpress";
+import { dev } from "$app/environment";
+
+export const BASE_URL = dev
+  ? "http://localhost/wordpress"
+  : "https://educaus.net/index.php";
 export const POSTS_API_URL = "/wp-json/wp/v2/posts";
