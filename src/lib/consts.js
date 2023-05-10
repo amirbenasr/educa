ebd854a717e7533c3b994d8917c8cb315d80443d
amir <amir@dragonchain.com>
2023-05-10 10:06:09 +0100

    change url

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,6 +1,4 @@
 import { dev } from "$app/environment";
 
-export const BASE_URL = dev
-  ? "http://localhost/wordpress"
-  : "https://educaus.net/index.php";
+export const BASE_URL = "https://educaus.net/index.php";
 export const POSTS_API_URL = "/wp-json/wp/v2/posts";
