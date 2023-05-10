c2f0b617b2d932dc89caffb6b6d383d24d9e2f28
amir <amir@dragonchain.com>
2023-05-10 13:01:43 +0100

    adhyst yrk

--- consts.js
+++ consts.js
@@ -2,5 +2,5 @@ import { dev } from "$app/environment";
 
 export const BASE_URL = dev
   ? "http://localhost/wordpress"
-  : "https://wordpress.educaus.net";
+  : "https://educaus.net/index.php";
 export const POSTS_API_URL = "/wp-json/wp/v2/posts";
