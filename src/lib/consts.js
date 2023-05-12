c60bdcae1bc9861bb93042b1fab1b8fdfaa31c4c
amir <amir@dragonchain.com>
2023-05-12 13:01:55 +0100

    test

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,6 +1,7 @@
 import { dev } from "$app/environment";
 
 export const BASE_URL = dev
-  ? "http://localhost/wordpress"
+  ? "https://wordpress.educaus.net"
   : "https://wordpress.educaus.net";
+
 export const POSTS_API_URL = "/wp-json/wp/v2/posts";
