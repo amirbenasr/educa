e88e0cd29fc42fbe41e90f0737cd3507a703e7a0
amir <amir@dragonchain.com>
2023-05-15 23:12:34 +0100

    flkeokaeee

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /home/wipd0555/nodevenv/educa/16/bin/activate && cd /home/wipd0555/educa
 npm install --save-dev && npm run build
+PORT=6100 npx pm2 start /home/wipd0555/educa/build/index.js
