91e3ef7ac7d757406b2f89fc50e9e9cd0fd4bef9
amir <amir@dragonchain.com>
2023-05-15 23:18:12 +0100

    flkeokaeee

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/wipd0555/nodevenv/educa/16/bin/activate && cd /home/wipd0555/educa
 npm install --save-dev && npm run build
-npx pm2 stop all
-PORT=6100 npx pm2 start /home/wipd0555/educa/build/index.js --name "educa bash"
+npx pm2 kill
+PORT=1337 npx pm2 start /home/wipd0555/educa/build/index.js --name "educa bash"
