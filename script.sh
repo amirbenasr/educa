ffbb0743a722774766a3b36c538d0ba9065d669f
amir <amir@dragonchain.com>
2023-05-15 23:04:32 +0100

    flkeoka

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/wipd0555/nodevenv/node.educaus.net/16/bin/activate && cd /home/wipd0555/educa
+source /home/wipd0555/nodevenv/educa/16/bin/activate && cd /home/wipd0555/educa
 npm install --save-dev && npm run build
