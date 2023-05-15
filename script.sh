cb66ce412ddae83607f206b9d39427356d1da621
amir <amir@dragonchain.com>
2023-05-15 22:17:53 +0100

    add script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/user/nodevenv/node.educaus.net/16/bin/activate && cd /home/user/educa
+source /home/wipd0555/nodevenv/node.educaus.net/16/bin/activate && cd /home/user/educa
 npm i && npm run build
