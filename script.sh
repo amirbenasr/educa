a744688fd5b243fb276ea6f85a6f38622502a827
amir <amir@dragonchain.com>
2023-05-16 11:59:20 +0100

    remove extra configuration

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source /home/wipd0555/nodevenv/educa/16/bin/activate && cd /home/wipd0555/educa
 npm install --save-dev && npm run build
-npx pm2 kill
-PORT=1337 npx pm2 start /home/wipd0555/educa/build/index.js --name "educa bash"
