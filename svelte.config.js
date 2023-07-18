2573f3896496855542d7f0199578cec78c968210
amir <amir@dragonchain.com>
2023-07-18 09:35:45 +0100

    switch adapter to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,12 @@
 import preprocessor from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 // import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  
   preprocess: preprocessor(),
   kit: {
-    
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    // adapter: adapter({
-    //   pages: "build",
-    //   assets: "build",
-    //   fallback: null,
-    //   precompress: false,
-    //   strict: true,
-    // }),
-    adapter: adapter({
-      fallback: "index.html", // 'index.html'
-    }),
+    adapter: adapter(),
   },
 };
 
